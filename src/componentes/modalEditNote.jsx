098a828945ce8d014fbe7f3e022b3bfc979d3a7e
unknown <chris.olivosf@gmail.com>
2023-05-30 11:25:05 -0400

    editando titulo y contenido

--- src/componentes/modalEditNote.jsx
+++ src/componentes/modalEditNote.jsx
@@ -23,7 +23,7 @@ function ModalEditNote(idNota, tituloNota, contenidoNota) {
 
   };
 
- // console.log('iniciales', valoresIniciales)
+  // console.log('iniciales', valoresIniciales)
 
   const [valores, setValores] = useState(valoresIniciales);
 
@@ -36,16 +36,16 @@ function ModalEditNote(idNota, tituloNota, contenidoNota) {
     setValores({ ...valores, [name]: value })
 
   }
-console.log(valores)
+  console.log(valores)
   const formOnSubmit = e => {
-   // console.log('submit')
+    // console.log('submit')
     e.preventDefault();
- //   console.log(e)
-//    console.log('submit')
+    //   console.log(e)
+    //    console.log('submit')
 
     editarNotaFuncion()
     async function editarNotaFuncion() {
-  //    console.log(arrayNote[0].idNota, valores.titulo, valores.contenido)
+      //    console.log(arrayNote[0].idNota, valores.titulo, valores.contenido)
 
       await editarNota({
         id: arrayNote[0].idNota,
@@ -56,10 +56,10 @@ console.log(valores)
 
       });
     }
-   setShow(false)
-  //   setValores(valoresIniciales)
-  e.target.reset()
-  //    console.log(valores)
+    setShow(false)
+    //   setValores(valoresIniciales)
+    e.target.reset()
+    //    console.log(valores)
 
   }
 
@@ -83,20 +83,16 @@ console.log(valores)
 
             <Form.Group className="mb-3"  >
               <Form.Label>Title </Form.Label>
-
               <Form.Control
-
                 name="titulo"
                 type="input"
-                value={arrayNote[0].tituloNota}
+                defaultValue={arrayNote[0].tituloNota}
                 autoFocus
                 onChange={cambiosTextoInput}
               />
             </Form.Group>
-            <Form.Group
-              className="mb-3"
-       
-            >
+
+            <Form.Group className="mb-3"  >
               <Form.Label>Content</Form.Label>
               <Form.Control as="textarea" rows={3}
                 name="contenido"
@@ -107,11 +103,11 @@ console.log(valores)
 
             </Form.Group>
             <Button variant="secondary" onClick={handleClose}>
-                Cancel
-              </Button>
-              <Button variant="success" type="submit" >
-                Save
-              </Button>
+              Cancel
+            </Button>
+            <Button variant="success" type="submit" >
+              Save
+            </Button>
           </Form>
         </Modal.Body>
         <Modal.Footer>
